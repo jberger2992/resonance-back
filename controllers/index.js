28ea94cf3959465fec0b3686796520d491c75393
jberger2992 <jberger2992@gmail.com>
2023-11-01 09:53:26 -0700

    bug

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const classRoutes = require("./classRoutes");
-router.use("/api/classes",classRoutes);
 const characterRoutes = require("./characterRoutes");
 router.use("/api/characters",characterRoutes);
 const setRoutes = require("./setRoutes");
